a4bef87ea9f655b5be03dc9df69819932e323574
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-14 17:17:04 +0200

    servicio, clases y estructura completo

---
+++ src/app/models/response.ts
@@ -0,0 +1,10 @@
+import { Book } from "./book";
+
+export class Response {
+    constructor(public error:boolean,
+                public code:number,
+                public message:string,
+                public data:Book[],
+                public result:null
+                ){}
+}
--- src/app/shared/books.service.ts
+++ src/app/shared/books.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../models/book';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  constructor() { }
+  private url = "https://api-books-theta.vercel.app/books";
+  constructor(private http: HttpClient){}
+
+  // Métodos del servicio
+  //-- Método para obtener todos los libros.
+  getAll(id_user:number):Observable<Object>{
+    return this.http.get(this.url);
+  }
+
+  //-- Método para obtener un solo libro --> buscador.
+  getOne(id_user:number,id_book:number):Observable<Object>{
+    return this.http.get(this.url + '?id_user=' + id_user + '&id_book=' + id_book);
+  }
+
+  //-- Método para añadir un libro --> Funcionalidad de la pg Add Book.
+  add(book:Book):Observable<Object>{
+    return this.http.post(this.url,book);
+  }
+
+  //-- Método para editar un libro --> Funcionalidad de la pg Update Book.
+  edit(book:Book):Observable<Object>{
+    return this.http.put(this.url,book);
+  }
+
+  //-- Método para borrar un libro --> Funcionalidad del botón 'X' de las cards de cada libro.
+  delete(id_book:number):Observable<Object>{
+    return this.http.request('delete', this.url, {body:{id_book:id_book}});
+  }
 }
