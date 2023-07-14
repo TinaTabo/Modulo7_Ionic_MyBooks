66192453de284e0b9b37e05a0b6942ca9bbfd557
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-14 12:59:16 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Modulo7_Ionic_MyBooks',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
